02751460f100684f6e2cfda410741b459a6e970f
miiinee <psmopen9357@gmail.com>
2019-06-13 16:12:38 +0900

    - fixed vulnerabilities(npm audit fix)

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,21 @@ router.use('/accounts', account);
 router.use('/books', book);
 router.use('/memos', memo);
 
+const request = require('request');
+
+router.get('/naver', (req, res) => {
+    request({
+        headers: {
+            'X-Naver-Client-Id' : 'LoAPjZT8ux7FDsA5bEB8',
+            'X-Naver-Client-Secret' : '7TjRCBCOLq'
+        },
+        uri: 'https://openapi.naver.com/v1/search/book.json',
+        qs: {
+          query: '명견',
+          display: 10,
+          start: 1
+        }
+      }).pipe(res);
+});
 
 module.exports = router;
